ef43fd3f8a8b834473f10f6b4d0e44476911126c
benjamin <benjamin@skyict.co.th>
2018-05-28 11:23:45 +0700

    Create MainFragment Success

--- app/src/main/java/benjamin/skyict/co/th/ticketservice/MainActivity.java
+++ app/src/main/java/benjamin/skyict/co/th/ticketservice/MainActivity.java
@@ -3,11 +3,26 @@ package benjamin.skyict.co.th.ticketservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import benjamin.skyict.co.th.ticketservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFranment, new MainFragment())
+                    .commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Class
